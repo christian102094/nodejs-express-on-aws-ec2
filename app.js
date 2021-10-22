a71e50cac2b1095fddcf51dac01c119951f36a89
Christian Tapia <christian1020_94@hotmail.com>
2021-10-22 01:26:06 -0500

    v2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.0</p>"
+    "<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 2.0</p>"
   );
 });
 app.get("/products", (req, res) => {
